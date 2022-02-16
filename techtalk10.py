5f31bf43b449d499dbc6369666e80526af79b3ea
Pranavi <pranukurti@gmail.com>
2022-02-16 12:52:51 -0800

    tpt file

---
+++ techtalk10.py
@@ -0,0 +1,9 @@
+
+#Write a python function to swap two numbers. Identify variable assignment in your code.
+day1 = 21
+day2 = 16
+print(day1,day2)
+# write a python function to swap age with lowest age first
+day1 = day2
+print(day1, day2)
+
